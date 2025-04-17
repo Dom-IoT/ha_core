294441e0b880cf198c46ba5fdd464e7840ffcc2a
thdemarty <48022911+thdemarty@users.noreply.github.com>
2025-04-17 22:12:09 +0200

    feat(back) route to list users from database fix #2

--- domiot_core/backend/main.py
+++ domiot_core/backend/main.py
@@ -22,9 +22,26 @@ def manual_sync():
     fetch_raw_users()
     return {"status": "ok", "message": "Sync started"}
 
+@app.get("/users")
+def list_users():
+    cur.execute("SELECT * FROM users")
+    rows = cur.fetchall()
+    users = []
+    for row in rows:
+        user = User(
+            username=row[1],
+            name=row[2],
+            is_owner=row[3],
+            is_active=row[4],
+            local_only=row[5],
+            domiot_role=row[6]
+        )
+        users.append(user)
+    return users
 
 
-@app.get("{username}")
+
+@app.get("/users/{username}")
 def get_user(username: str):
 
     cur.execute("SELECT * FROM users WHERE username=?", (username,))
